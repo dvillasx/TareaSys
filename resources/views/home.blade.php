92a26c0bf648282a4af2324392e955e4ee0c5147
Damian <damian.alf.villa.cis@gmail.com>
2020-03-19 17:14:29 -0600

    Temas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,5 @@
-@extends('layouts.app')
+@extends('layouts.tema')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <h1 class="h3 mb-4 text-gray-800">Mi Blank Page</h1>
 @endsection
